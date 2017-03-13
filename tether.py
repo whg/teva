2da8de0c021625de8a785803b133ddc276bdb324
Will Gallia <will@stillatplay.com>
2017-03-13 14:41:26 +0000

    just look for 4 cameras, forget about serial numbers

--- tether.py
+++ tether.py
@@ -2,26 +2,21 @@ import subprocess
 import gphoto2cffi as gp
 import sys
 
-cameras = {
-   '00000000000000000000000006031485' : 'C',
-   '00000000000000000000000006020701' : 'A',
-   '00000000000000000000000006006782' : 'D',
-   '00000000000000000000000006020585' : 'B',
-}
-
 cams = gp.list_cameras()
-mappings = { c.status.serialnumber : c._usb_address for c in cams }
 
-output = ""
-for serial, address in mappings.items():
-    output+= "pushd /Volumes/ro0kie\\ HDD/_PROJECTS/IKEA_tests/CM{}/\n".format(cameras[serial])
-    output+= 'gphoto2 --capture-tethered --port "usb:{:03d},{:03d}" --keep-raw --force-overwrite&\n'.format(*address)
+folders = ['A', 'B', 'C', 'D']
+ports = [c._usb_address for c in cams]
+
+if len(folders) != len(ports):
+    print("Note: Camera mismatch, found {}, looking for {}".format(len(ports), len(folders)))
+
+output = 'dir=$(dirname "$0")\n'
+for folder, port in zip(folders, ports):
+    output+= "pushd $dir/CM{}/\n".format(folder)
+    output+= 'gphoto2 --capture-tethered --port "usb:{:03d},{:03d}" --keep-raw --force-overwrite&\n'.format(*port)
     output+= "popd\n"
+    print("Folder {} being used for port {}".format(folder, port))
 
-for serial in cameras.keys():
-    if serial not in mappings:
-        output+= "# {} not found".format(serial)
-        print("{} NOT FOUND!!!!!".format(serial))
 
-with open('tether.sh', 'w') as f:
+with open('tether', 'w') as f:
     f.write(output)
